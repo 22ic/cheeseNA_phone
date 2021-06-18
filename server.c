f226f2193ddf0f48ae848e874f221614ef54e02e
cheeseNA <kurocat2000@gmail.com>
2021-06-18 21:55:30 +0900

    Revert "Add thread"
    This reverts commit 4151cd9537bb609ed89650fc59d5366e36cf093d.

--- server.c
+++ server.c
@@ -3,7 +3,6 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -14,8 +13,6 @@ void die(char* s) {
   perror(s);
   exit(1);
 }
-
-void com(void* s);
 
 int main(int argc, char** argv) {
   if (argc != 2) die("./server <port>");
@@ -30,43 +27,30 @@ int main(int argc, char** argv) {
   int ret = bind(ss, (struct sockaddr*)&addr, sizeof(addr));
   if (ret == -1) die("bind");
 
-  pthread_t thread;
-
   ret = listen(ss, 10);
   if (ret == -1) die("listen");
 
   struct sockaddr_in client_addr;
   socklen_t len = sizeof(struct sockaddr_in);
-  int s;
-  while (1) {
-    s = accept(ss, (struct sockaddr*)&client_addr, &len);
-    if (s == -1) die("socket2");
+  int s = accept(ss, (struct sockaddr*)&client_addr, &len);
+  if (s == -1) die("socket2");
 
-    ret = pthread_create(&thread, NULL, (void*)com, (void*)&s);
-    if (ret != 0) die("pthread_create");
-    ret = pthread_detach(thread);
-    if (ret != 0) die("pthread_detach");
-  }
-  // close(ss);
-  return 0;
-}
+  close(ss);
 
-void com(void* s) {
-  int sock = *((int*)s);
-  printf("%d\n", sock);
   int bufsize = 1024 * 2;
   unsigned char buf[bufsize];
   int n;
 
   while (1) {
-    n = recv(sock, buf, bufsize, 0);
+    n = recv(s, buf, bufsize, 0);
     // printf("n: %d\n", n);
     if (n == -1) die("recv");
     if (n == 0) break;
-    n = send(sock, buf, n, 0);
+    n = send(s, buf, n, 0);
     if (n == -1) die("send");
   }
 
-  close(sock);
-  return;
+  close(s);
+
+  return 0;
 }
